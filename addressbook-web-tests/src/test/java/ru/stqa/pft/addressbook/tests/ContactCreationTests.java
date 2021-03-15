42deae404b7a2dd853feb6b657d466d09aa4d1e3
meshcheryakova <mesheryakovaelenav@gmail.com>
2021-03-16 00:23:44 +0300

    Создали генератор тестовых данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -13,13 +15,15 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
     app.goTo().contactPage();
     Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/stru.png");
     ContactData contact = new ContactData()
             .withFirstname("Petr")
             .withLastname("Petrovich")
             .withAddress("St. Petersburg")
             .withHomePhone("245-98-12")
             .withAllEmail("petr@mail.ru")
-            .withGroup("test2");
+            .withGroup("test2")
+            .withPhoto(photo);
     app.contact().create(contact, true);
     Contacts after = app.contact().all();
 
@@ -29,6 +33,15 @@ public class ContactCreationTests extends TestBase {
             before.withAdded(contact.withId(after.stream().mapToInt(ContactData::getId).max().getAsInt()))));
 
   }
+  /*
+  @Test
+  public void testCurrentDir() throws Exception {
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsolutePath());
+    File photo = new File("src/test/resources/stru.png");
+    System.out.println(photo.getAbsolutePath());
+    System.out.println(photo.exists());
+  }*/
 
 
 }
